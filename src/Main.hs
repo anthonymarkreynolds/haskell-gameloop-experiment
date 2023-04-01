94d590c4c1ca0fb7661badb71ee81dd51926e192
anthonymarkreynolds <anthonymarkreynolds@outlook.com>
2023-04-01 11:13:28 +1000

    implement menuItem actions and menu navigation

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,14 @@
-{-# LANGUAGE LambdaCase      #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Main (main) where
 
 import           Algebra.Graph.Undirected
 import           Control.Lens
-import           Control.Monad.State      (StateT, evalStateT, liftIO)
+import           Control.Monad.State      (StateT, evalStateT, forM_, liftIO,
+                                           unless)
 import           System.Console.ANSI      (clearScreen, hideCursor, showCursor)
 import           System.IO                (BufferMode (LineBuffering, NoBuffering),
                                            hSetBuffering, hSetEcho, stdin)
-import           Text.Read                (readEither, readMaybe)
+import           Text.Read                (readMaybe)
 
 getCharNoEcho :: IO Char
 getCharNoEcho = do
@@ -19,17 +19,24 @@ getCharNoEcho = do
   hSetEcho stdin True
   return c
 
-data Message = Info String | Alert String | Plain String deriving (Show)
+data Message
+  = Info String
+  | Alert String
+  | Plain String
+  deriving (Show)
 
-newtype MenuAction = MenuAction { _action :: StateT AppState IO () }
+newtype Action = Action { _action :: StateT AppState IO () }
 
-type MenuItem = (Menu, Maybe MenuAction)
+data MenuItem = MenuItem {
+  _itemLabel  :: String,
+  _nextMenu   :: Maybe Menu,
+  _itemAction :: Maybe Action
+}
 
 data Menu = Menu {
   _menuName  :: String,
   _menuItems :: [MenuItem]
 }
-
 
 type Room = String
 data Game = Game {
@@ -41,62 +48,75 @@ data Game = Game {
 data AppState = AppState {
   _notice      :: Message,
   _currentMenu :: Menu,
-  _currentGame :: Game
+  _currentGame :: Game,
+  _quit        :: Bool
 }
 makeLenses ''Menu
+makeLenses ''MenuItem
 makeLenses ''Game
 makeLenses ''AppState
-makeLenses ''MenuAction
+makeLenses ''Action
 
+
+
+quitMenuItem :: MenuItem
+quitMenuItem = MenuItem "Quit" Nothing (Just (Action (quit .= True)))
+
+
+testAction :: Action
+testAction = Action $ currentGame . playerAge += 1
 
 mainMenu :: Menu
-mainMenu = Menu "Main" [ (helpMenu, Nothing), (mainMenu, Nothing) ]
+mainMenu = Menu "Main"
+  [ MenuItem "Help Menu"     (Just helpMenu) Nothing
+  , MenuItem "Increment age" Nothing         (Just testAction)
+  , quitMenuItem ]
 
 helpMenu :: Menu
-helpMenu = Menu "Help" [ (mainMenu, Nothing) ]
-
--- parseInput :: String -> Either String Int
--- parseInput s = case readEither s of
---   Just i -> Right i
---   Nothing -> Left "Invalid input"
+helpMenu = Menu "Help" [ MenuItem "Main Menu" (Just mainMenu) Nothing ]
 
 renderMessage :: Message -> String
-renderMessage = \case
-  Info  str -> "🛈 " ++ str ++ "  "
-  Alert str -> "⚠ " ++ str ++ "  "
-  Plain str -> str
+renderMessage (Info str)  = "🛈 " ++ str ++ "  "
+renderMessage (Alert str) = "⚠ " ++ str ++ "  "
+renderMessage (Plain str) = str
+
+
 
 runGameLoop :: StateT AppState IO ()
 runGameLoop = do
-  menu <- use currentMenu
-  flash <- use notice
-  game <- use currentGame
-  let items = menu ^. menuItems
+  quitFlag <- use quit
+  unless quitFlag $ do
+    menu <- use currentMenu
+    flash <- use notice
+    game <- use currentGame
+    let items = menu ^. menuItems
 
-  -- Render the view
-  liftIO $ do
-    clearScreen
-    putStrLn $ renderMessage flash
-    putStrLn $ "Menu: " ++ menu ^. menuName
-    putStr $ ifoldMap (\i item -> show (i + 1) ++ ". " ++ (_menuName . fst) item ++ "\n") items
+    -- Render the view
+    liftIO $ do
+      clearScreen
+      putStrLn $ "Age: " ++ show (game ^. playerAge)
+      putStrLn $ renderMessage flash
+      putStrLn $ "Menu: " ++ menu ^. menuName
+      putStr $ ifoldMap (\i item -> show (i + 1) ++ ". " ++ (item ^. itemLabel) ++ "\n") items
 
-  c <- liftIO getCharNoEcho
-  case readMaybe [c] :: Maybe Int of
-    Just i -> do
-      case items ^? ix (i - 1) of
-        Just (menu', Nothing) -> do
-          currentMenu .= menu'
-          notice .= Plain ("You selected " ++ show i ++ ". " ++ menu' ^. menuName )
-        Just (menu', Just action) -> return ()
-          -- not sure how to run a menu action
+    c <- liftIO getCharNoEcho
+    case readMaybe [c] :: Maybe Int of
+      Just i -> do
+        case items ^? ix (i - 1) :: Maybe MenuItem of
+          Just selectedItem -> do
+            forM_ (selectedItem ^. nextMenu) (currentMenu .=)
+            forM_ (selectedItem ^. itemAction) _action
+            notice .=  Plain ("You selected " ++ show i ++ ". " ++ selectedItem ^. itemLabel)
 
-        Nothing -> notice .= Info ("No menu item for " ++ show i)
-    Nothing -> notice .= Alert "Invalid input"
+          Nothing -> notice .= Info ("No menu item for " ++ show i)
+      Nothing -> notice .= Alert "Invalid input"
 
-  runGameLoop
+    runGameLoop
 
 main :: IO ()
 main = do
   hideCursor
-  evalStateT runGameLoop $ AppState (Info "Welcome") mainMenu $ Game "noname" 30 $ vertex "test"
+  evalStateT runGameLoop $ AppState (Info "Welcome")  mainMenu (Game "noname" 30 (vertex "test")) False
   showCursor
+  clearScreen
+  putStrLn "Thanks for playing, goodbye!"
